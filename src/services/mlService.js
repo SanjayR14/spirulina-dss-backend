6bcc55d5949747a82153fe53ab05f6f4b2321f76
sanjay <sanjusaran1435@gmail.com>
2026-03-13 00:00:25 +0530

    correct

--- src/services/mlService.js
+++ src/services/mlService.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const ML_SERVICE_URL = "https://spirulina-dss-ml.onrender.com";
+const ML_SERVICE_URL = "https://spirulina-dss-ml.onrender.com/api/v1/site";
 
 const analyzeSite = async (location) => {
   const response = await axios.get(`${ML_SERVICE_URL}/analyze`, {
